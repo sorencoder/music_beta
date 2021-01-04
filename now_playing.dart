cd5c8149494e2a1d2bcfa27ba54989d60f16ad20
sorencoder <69846992+sorencoder@users.noreply.github.com>
2021-01-04 22:33:01 +0530

    Update now_playing.dart

--- now_playing.dart
+++ now_playing.dart
@@ -7,16 +7,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+         theme: ThemeData(
+    accentTextTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
+  ),
         debugShowCheckedModeBanner: false,
-        home: HomePage());
+        home: PlayerPage());
   }
 }
 
-class HomePage extends StatelessWidget {
+class PlayerPage extends StatelessWidget {
+  final rating = 20.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,15 +31,16 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Container(
+//         color: Colors.blue,
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Colors.white,
         child: Column(
           children: <Widget>[
-            SizedBox(height:5),
+            SizedBox(height: 5),
             Container(
-                height:150,
-                  width:150,
+              height: 150,
+              width: 150,
 //               padding: EdgeInsets.all(8.0),
               decoration: BoxDecoration(
                 image: DecorationImage(
@@ -46,6 +49,42 @@ class HomePage extends StatelessWidget {
                 ),
                 borderRadius: BorderRadius.circular(5.0),
               ),
+            ),
+            SizedBox(height: 10),
+            Text('Song Name',
+                style: TextStyle(color: Colors.black, fontSize: 30)),
+//             Container(
+// //               height:100,
+// //               width:100,
+// //               color: Colors.red,
+//               child: Text('Song Name',
+//                   style: TextStyle(color: Colors.black, fontSize: 30)),
+//             ),
+            SizedBox(height: 10),
+            Text(
+              'Artist Name',
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            ),
+            SizedBox(height: 55),
+            Slider(
+              onChanged: null,
+              inactiveColor: Colors.black,
+              value: rating,
+              max: 100,
+              min: 0,
+//               label: rating.toString(),
+              activeColor: Colors.black,
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Icon(Icons.favorite,color:Colors.black),
+                Icon(Icons.arrow_back,color:Colors.black),
+                Icon(Icons.play_circle_fill_rounded,size:60,color:Colors.black),
+                Icon(Icons.arrow_forward,color:Colors.black),
+                Icon(Icons.shuffle,color:Colors.black)
+              ],
             ),
           ],
         ),
